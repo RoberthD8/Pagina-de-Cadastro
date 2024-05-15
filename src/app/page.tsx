c49bb0aab27683e2a1d1a4d6b3a4f71b8b30f558
Roberth Souza <xroberthd8@gmail.com>
2024-05-15 10:41:02 -0300

    Css incluso

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,11 @@
 
 import type { NextPage } from 'next';
 import { useState } from 'react';
+import './globals.css'
 
 const Home: NextPage = () => {
   const [formData, setFormData] = useState({
     nome: '',
-    email: '',
     email: '',
     senha: ''
   });
@@ -34,28 +34,30 @@ const Home: NextPage = () => {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <form className="p-8 bg-white rounded shadow-md" onSubmit={handleSubmit}>
         <h1 className="text-lg font-bold text-center mb-6">Cadastro</h1>
+        <br/>
         <div className="mb-4">
-          <label htmlFor="nome" className="block text-sm font-medium text-gray-700">Nome</label>
+          <label htmlFor="nome" className="block text-sm font-medium text-gray-700">Nome: </label>
           <input type="text" id="nome" name="nome" required
                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                  onChange={handleChange}
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email: </label>
           <input type="email" id="email" name="email" required
                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                  onChange={handleChange}
           />
         </div>
         <div className="mb-6">
-          <label htmlFor="senha" className="block text-sm font-medium text-gray-700">Senha</label>
+          <label htmlFor="senha" className="block text-sm font-medium text-gray-700">Senha: </label>
           <input type="password" id="senha" name="senha" required
                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                  onChange={handleChange}
           />
         </div>
         <button type="submit" className="w-full bg-indigo-600 text-white px-3 py-2 rounded hover:bg-indigo-700">Cadastrar</button>
+        
       </form>
     </div>
   )
